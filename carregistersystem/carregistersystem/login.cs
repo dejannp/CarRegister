dfdaf115bb862b27e809c11bd0fd99cfa9eb918c
dejannp <pavicdejan005@gmail.com>
2024-09-27 02:51:56 +0200

    konekcija na bazu, kreiranje CarRegister i tabela

--- carregistersystem/carregistersystem/login.cs
+++ carregistersystem/carregistersystem/login.cs
@@ -9,11 +9,93 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace carregistersystem
 {
+
+    
     public partial class login : Form
     {
+        IniFile ini = new IniFile(@"..\\..\\include\\config.ini");
+        string username, provider, password,db;
+        bool databaseCreated;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            ini.UpdateProperty("Server", textBox1.Text, "DatabaseConfig");
+            ini.UpdateProperty("Username", textBox2.Text, "DatabaseConfig");
+            ini.UpdateProperty("Password", textBox3.Text, "DatabaseConfig");
+
+            provider = ini.Read("DatabaseConfig", "Server");
+            username = ini.Read("DatabaseConfig", "Username");
+            password = ini.Read("DatabaseConfig", "Password");
+            db = ini.Read("DatabaseConfig", "Database");
+
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = $"Data Source={provider};Initial Catalog={db};User id={username};Password={password};";
+            try
+            {
+                conn.Open();
+
+                MessageBox.Show("RADI");
+
+            }
+            catch
+            {
+                MessageBox.Show("GRESKA!");
+
+            }
+
+            databaseCreated = Convert.ToBoolean(ini.Read("DatabaseConfig", "DatabaseCreated"));
+
+            if (!databaseCreated)
+
+
+
+            {
+                //kreirajbazu
+                string createdbquerytext,createtablesquerytext;
+                createdbquerytext = File.ReadAllText(@"..\\..\\include\\createdb.txt");
+                createtablesquerytext = File.ReadAllText(@"..\\..\\include\\createtables.txt");
+
+
+                SqlCommand createdbquery = new SqlCommand(createdbquerytext,conn);
+                SqlCommand createtablesquery = new SqlCommand(createtablesquerytext, conn);
+
+                createdbquery.ExecuteNonQuery();
+                createtablesquery.ExecuteNonQuery();
+
+
+
+
+                ini.UpdateProperty("DatabaseCreated", "true", "DatabaseConfig");
+
+
+               
+
+
+
+
+
+
+
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
         public login()
         {
             InitializeComponent();
@@ -22,18 +104,30 @@ namespace carregistersystem
         private void login_Load(object sender, EventArgs e)
         {
          
-            IniFile ini = new IniFile(@"..\\..\\include\\config.ini");
-           
-             /*INI FILE PATH CHECK
-            string read;
-            read = ini.Read("DatabaseConfig", "username");
-            label1.Text = read;
-           */
+            
+
+            /*INI FILE PATH CHECK
+           string read;
+           read = ini.Read("DatabaseConfig", "username");
+           label1.Text = read;
+          */
+
+            username = ini.Read("DatabaseConfig", "Username");
+            password = ini.Read("DatabaseConfig", "Password");
+            db = ini.Read("DatabaseConfig", "Database");
+            provider = ini.Read("DatabaseConfig","Server");
+
+            textBox1.Text = provider;
+            textBox2.Text = username;
+            textBox3.Text = password;
 
 
 
 
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
